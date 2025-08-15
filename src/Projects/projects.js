84e95ae89a2f1b0dd673203063b7fb1426643dda
Taniadfs <t.dangelofonfria@gmail.com>
2025-08-15 21:39:27 +0200

    skills mas pequenas yenlances ok href proyectos

--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -13,20 +13,20 @@ export function renderProjects() {
       <div class="project-item">
       <img src ="./assets/fotos projects/Landin page .png" alt="Project 1" class="project-image">
         <h3>Landing Page</h3>
-        <p class="project-description">Description of project 1.</p>
-        <a hrf="#" class="project-button">View Project</a>
+        <p class="project-description">Landing page para la marca de joyas Jimmuses</p>
+        <a href="https://spiffy-dango-5cc085.netlify.app/" target="_blank" rel="noopener noreferrer" class="project-button">View Project</a>
       </div>
       <div class="project-item">
       <img src ="./assets/fotos projects/WEB JIMMUSES.png" alt="Project 2" class="project-image">
         <h3>Web JIMMUSES</h3>
-        <p class="project-description">Description of project 2.</p>
-        <a hrf="#" class="project-button">View Project</a>
+        <p class="project-description">Web para la marca de joyas Jimmuses</p>
+        <a href="https://delightful-pony-695275.netlify.app/" target="_blank" rel="noopener noreferrer" class="project-button">View Project</a>
       </div>
       <div class="project-item">
       <img src ="./assets/fotos projects/Proyecto inpisterest.png" alt="Project 3" class="project-image">
         <h3>App Inspinterest</h3>
-        <p class="project-description">Description of project 3.</p>
-        <a hrf="#" class="project-button">View Project</a>
+        <p class="project-description">Replica de una pagina App como Pinterest</p>
+        <a href="https://inspinterest.netlify.app/" target="_blank" rel="noopener noreferrer" class="project-button">View Project</a>
       </div>
     </div>
   </div>
