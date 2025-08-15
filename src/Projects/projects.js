bf93442005a74a63dc3864087ab546d19dd20f27
Taniadfs <t.dangelofonfria@gmail.com>
2025-08-15 11:50:14 +0200

    projects imagenes insertadas

--- src/Projects/projects.js
+++ src/Projects/projects.js
@@ -11,21 +11,21 @@ export function renderProjects() {
     <p class="projects-subtitle">Some things I’ve been cooking so far</p>
     <div class="projects-container-grid">
       <div class="project-item">
-      <img src ="" alt="Project 1" class="project-image">
-        <h3>Project 1</h3>
+      <img src ="./assets/fotos projects/Landin page .png" alt="Project 1" class="project-image">
+        <h3>Landing Page</h3>
         <p class="project-description">Description of project 1.</p>
         <a hrf="#" class="project-button">View Project</a>
       </div>
       <div class="project-item">
-      <img src ="" alt="Project 1" class="project-image">
-        <h3>Project 2</h3>
-        <p>Description of project 2.</p>
+      <img src ="./assets/fotos projects/WEB JIMMUSES.png" alt="Project 2" class="project-image">
+        <h3>Web JIMMUSES</h3>
+        <p class="project-description">Description of project 2.</p>
         <a hrf="#" class="project-button">View Project</a>
       </div>
       <div class="project-item">
-      <img src ="" alt="Project 1" class="project-image">
-        <h3>Project 3</h3>
-        <p>Description of project 3.</p>
+      <img src ="./assets/fotos projects/Proyecto inpisterest.png" alt="Project 3" class="project-image">
+        <h3>App Inspinterest</h3>
+        <p class="project-description">Description of project 3.</p>
         <a hrf="#" class="project-button">View Project</a>
       </div>
     </div>
